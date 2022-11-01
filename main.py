d2ba6cab013cf304185e3b297fcd5cf0c8ee9563
hallie <hhpham218@gmail.com>
2022-11-01 01:37:27 -0700

    included a UI and integrated my heuristics

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from queue import Queue
 from treelib import Node, Tree
 import copy
 import heapq
+import numpy
 
 goal_state = [[1, 2, 3],
               [4, 5, 6],
@@ -12,29 +13,41 @@ goal_state_coordinates = {1: [0, 0], 2: [0, 1], 3: [0, 2],
                           4: [1, 0], 5: [1, 1], 6: [1, 2],
                           7: [2, 0], 8: [2, 1], 0: [2, 2]}
 
-depth_1 = [[1, 2, 3],
-           [4, 5, 6],
-           [7, 0, 8]]
 
-depth_2 = [[1, 2, 3],
-           [4, 5, 6],
-           [0, 7, 8]]
+class Board:
+    def __init__(self, node: list[list[int]]):
+        self.node = node
+        self.cost = 0
+        self.depth = 0
 
-depth_4 = [[1, 2, 3],
-           [5, 0, 6],
-           [4, 7, 8]]
+    def __lt__(self, other):
+        return self.cost < other.cost
 
-depth_8 = [[1, 3, 6],
-           [5, 0, 2],
-           [4, 7, 8]]
 
-depth_16 = [[1, 6, 7],
-            [5, 0, 3],
-            [4, 8, 2]]
+depth_1 = Board([[1, 2, 3],
+                 [4, 5, 6],
+                 [7, 0, 8]])
+
+depth_2 = Board([[1, 2, 3],
+                 [4, 5, 6],
+                 [0, 7, 8]])
+
+depth_4 = Board([[1, 2, 3],
+                 [5, 0, 6],
+                 [4, 7, 8]])
+
+depth_8 = Board([[1, 3, 6],
+                 [5, 0, 2],
+                 [4, 7, 8]])
+
+depth_16 = Board([[1, 6, 7],
+                  [5, 0, 3],
+                  [4, 8, 2]])
 
 test = [[1, 2, 4],
         [3, 0, 6],
         [7, 8, 5]]
+
 
 def print_puzzle(puzzle):
     for i in range(0, 3):
@@ -97,7 +110,6 @@ def misplaced_tile(node):  # resulting count is the g(n) for the misplaced tile 
 
 def manhattan_distance(node):  # the g(n) is the sum of the distance all misplaced tiles are from their goal spot
     # dictionary with coordinates of the goal states is goal_state_coordinates
-    # create a dictionary with all current spots of the node that is passed in
     # do the math aka find displacement of all the missing ones and then sum it
     distance_sum = 0
     for j in range(3):
@@ -118,30 +130,31 @@ def find_zero(initial_node):
                 return j, i
 
 
-def general_search(initial_problem):
-    # queue = []
-    queue = Queue(maxsize=0)
-    # heapq.heapify(queue)
-    # heapq.heappush(queue, initial_problem)
-    queue.put(initial_problem)
+def general_search(algo, initial_problem):
+    queue = []
+    heapq.heapify(queue)
+    heapq.heappush(queue, (0, initial_problem.node))
+    # queue.put(initial_problem)
     tree = Tree()
     parent_num = 0
     tree.create_node(initial_problem, 1)  # root node
+    visited_nodes = []
     expanded_nodes = []
     expanded_nodes_count = 0
     max_queue_size = 0
     tree_id = 2
 
-    while queue.qsize() > 0:
-        max_queue_size = max(queue.qsize(), max_queue_size)
-        # current_node = heapq.heappop(queue)
-        current_node = queue.get()
+    while len(queue) > 0:
+        max_queue_size = max(len(queue), max_queue_size)
+        current_node = heapq.heappop(queue)
+        current_node = current_node[1]
+        # current_node = queue.get()
         # print_puzzle(current_node)
         parent_num += 1
         if current_node == goal_state:
             print("Goal state reached!")
-            #while len(expanded_nodes) > 0:
-                #print_puzzle(expanded_nodes.pop())
+            # while len(expanded_nodes) > 0:
+            # print_puzzle(expanded_nodes.pop())
             print('expanded tree:')
             tree.show()
             print("Number of nodes expanded: ", expanded_nodes_count)
@@ -149,8 +162,9 @@ def general_search(initial_problem):
             # print('expanded nodes:')
             # print(expanded_nodes)
             # print('depth of tree: ', tree.depth())
-            pls = tree.get_node(tree_id)
-            print('depth of tree: ', tree.depth(pls))
+            # pls = tree.get_node(tree_id)
+            # print('depth of tree: ', tree.depth(pls))
+            print('depth of tree: ', tree.depth())
             return current_node
         else:
             zeroth = find_zero(current_node)
@@ -165,7 +179,15 @@ def general_search(initial_problem):
                     if temp3 not in expanded_nodes:
                         tree.create_node(temp3, tree_id, parent=parent_num)
                         tree_id += 1
-                        queue.put(temp3)
+                        # queue.put(temp3)
+                        temp_depth = tree.depth()
+                        if algo == 1:
+                            cost = temp_depth
+                        elif algo == 2:
+                            cost = temp_depth + misplaced_tile(temp3)
+                        elif algo == 3:
+                            cost = temp_depth + manhattan_distance(temp3)
+                        heapq.heappush(queue, (cost, temp3))
             except:
                 print('tile cannot move right')
             try:
@@ -175,7 +197,15 @@ def general_search(initial_problem):
                         tree.create_node(temp4, tree_id, parent=parent_num)
                         tree_id += 1
                         # heapq.heappush(queue, temp4)
-                        queue.put(temp4)
+                        # queue.put(temp4)
+                        temp_depth = tree.depth()
+                        if algo == 1:
+                            cost = temp_depth
+                        elif algo == 2:
+                            cost = temp_depth + misplaced_tile(temp4)
+                        elif algo == 3:
+                            cost = temp_depth + manhattan_distance(temp4)
+                        heapq.heappush(queue, (cost, temp4))
             except:
                 print('tile cannot move left')
             try:
@@ -185,7 +215,15 @@ def general_search(initial_problem):
                         tree.create_node(temp, tree_id, parent=parent_num)
                         tree_id += 1
                         # heapq.heappush(queue, temp)
-                        queue.put(temp)
+                        # queue.put(temp)
+                        temp_depth = tree.depth()
+                        if algo == 1:
+                            cost = temp_depth
+                        elif algo == 2:
+                            cost = temp_depth + misplaced_tile(temp)
+                        elif algo == 3:
+                            cost = temp_depth + manhattan_distance(temp)
+                        heapq.heappush(queue, (cost, temp))
             except:
                 print('tile cannot move down')
             try:
@@ -194,17 +232,25 @@ def general_search(initial_problem):
                     if temp2 not in expanded_nodes:
                         tree.create_node(temp2, tree_id, parent=parent_num)
                         tree_id += 1
-                        # heapq.heappush(queue, temp2)
-                        queue.put(temp2)
+                        temp_depth = tree.depth()
+                        if algo == 1:
+                            cost = temp_depth
+                        elif algo == 2:
+                            cost = temp_depth + misplaced_tile(temp2)
+                        elif algo == 3:
+                            cost = temp_depth + manhattan_distance(temp2)
+                        heapq.heappush(queue, (cost, temp2))
+                        # queue.put(temp2)
             except:
                 print('tile cannot move up')
-            #print(queue[0])
+            # print(queue[0])
             expanded_nodes_count += 1
 
-    if queue.qsize() == 0:
+    if len(queue) == 0:
         print('Search failed. There is no solution to this puzzle!')
 
 # general_search(depth_2)
+
 
 '''current_node = copy.deepcopy(depth_2)
 print_puzzle(current_node)
@@ -226,7 +272,18 @@ if yet_another == None:
 tree.show()
 print_puzzle(current_node)'''
 
-# general_search(depth_16)
 
-print('for misplaced tile g(n) =', misplaced_tile(test))
-print('for manhattan g(n) =', manhattan_distance(test))
+def main_menu():
+    print('Welcome to 8 Puzzle Solver!')
+    algo = input('Select your algorithm. \n'
+                 '(1) for Uniform Cost \n'
+                 '(2) for Misplaced Tile Heuristic \n'
+                 '(3) for Manhattan Distance Heuristic \n')
+    algo = int(algo)
+    general_search(algo, depth_2)
+
+
+main_menu()
+
+# print('for misplaced tile g(n) =', misplaced_tile(depth_8))
+# print('for manhattan g(n) =', manhattan_distance(depth_8))
